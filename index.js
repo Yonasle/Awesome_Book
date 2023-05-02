e5e08d8f49c26f4cced67b7f9d7f838b7703f597
Gergito Kusse <gergito2@gmail.com>
2023-05-02 17:39:44 +0300

    Update for linters error

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // Check if local storage is empty, then add an empty array
 
-if (localStorage.getItem("addedBooks") === null) {
-  localStorage.setItem("addedBooks", JSON.stringify([]));
+if (localStorage.getItem('addedBooks') === null) {
+  localStorage.setItem('addedBooks', JSON.stringify([]));
 }
 
 // Store data into local storage
-const storeData = JSON.parse(localStorage.getItem("addedBooks"));
+const storeData = JSON.parse(localStorage.getItem('addedBooks'));
 
 function updateData() {
-  localStorage.setItem("addedBooks", JSON.stringify(storeData));
+  localStorage.setItem('addedBooks', JSON.stringify(storeData));
 }
 
 // Getting values from input fields
 
 // Display books to the UI from local storage
 function displayBooks() {
-  const listOfBooks = document.querySelector(".container");
-  listOfBooks.innerHTML = "";
+  const listOfBooks = document.querySelector('.container');
+  listOfBooks.innerHTML = '';
   storeData.forEach((book, i) => {
-    const bookItem = document.createElement("li");
-    bookItem.style.listStyleType = "none"; // add this line to remove the bullet points
+    const bookItem = document.createElement('li');
+    bookItem.style.listStyleType = 'none'; // add this line to remove the bullet points
     bookItem.innerHTML = `
       <p>${book.title}</p>
       <p>${book.author}</p>
@@ -41,11 +41,11 @@ function addNewData(bookTitle, bookAuthor) {
     displayBooks();
   }
 }
-const form = document.querySelector("form");
-form.addEventListener("submit", (e) => {
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const title = document.querySelector(".title").value;
-  const author = document.querySelector(".author").value;
+  const title = document.querySelector('.title').value;
+  const author = document.querySelector('.author').value;
   addNewData(title, author);
 });
 
