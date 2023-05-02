726530dc750a6e5dd6b4b458cf55d1b8400b8f08
Yonas Lemma <lemmayonas@gmail.com>
2023-05-02 14:07:16 +0300

    add and delete booklist

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // Check if local storage is empty, then add an empty array
 
-if (localStorage.getItem("addedBooks") === null) {
-  localStorage.setItem("addedBooks", JSON.stringify([]));
+if (localStorage.getItem('addedBooks') === null) {
+  localStorage.setItem('addedBooks', JSON.stringify([]));
 }
 
 // Store data into local storage
-const storeData = JSON.parse(localStorage.getItem("addedBooks"));
+const storeData = JSON.parse(localStorage.getItem('addedBooks'));
 
 function updateData() {
-  localStorage.setItem("addedBooks", JSON.stringify(storeData));
+  localStorage.setItem('addedBooks', JSON.stringify(storeData));
 }
 
 // Getting values from input fields
 
 // Display books to the UI from local storage
 function displayBooks() {
-  const listOfBooks = document.querySelector(".container");
-  listOfBooks.innerHTML = "";
+  const listOfBooks = document.querySelector('.container');
+  listOfBooks.innerHTML = '';
   storeData.forEach((book, i) => {
-    const bookItem = document.createElement("li");
+    const bookItem = document.createElement('li');
     bookItem.innerHTML = `
       <p>${book.title}</p>
       <p>${book.author}</p>
@@ -38,11 +38,11 @@ function addNewData(bookTitle, bookAuthor) {
   updateData();
   displayBooks();
 }
-const form = document.querySelector("form");
-form.addEventListener("submit", (e) => {
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const title = document.querySelector(".title").value;
-  const author = document.querySelector(".author").value;
+  const title = document.querySelector('.title').value;
+  const author = document.querySelector('.author').value;
   addNewData(title, author);
 });
 
