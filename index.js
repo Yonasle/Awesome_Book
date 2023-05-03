2a2072efb34f7007e57b6c985fedb70713ba010c
Yonas Lemma <lemmayonas@gmail.com>
2023-05-03 10:06:06 +0300

    add style part

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 // Check if local storage is empty, then add an empty array
 
-if (localStorage.getItem('addedBooks') === null) {
-  localStorage.setItem('addedBooks', JSON.stringify([]));
+if (localStorage.getItem("addedBooks") === null) {
+  localStorage.setItem("addedBooks", JSON.stringify([]));
 }
 
 // Store data into local storage
-const storeData = JSON.parse(localStorage.getItem('addedBooks'));
+const storeData = JSON.parse(localStorage.getItem("addedBooks"));
 
 function updateData() {
-  localStorage.setItem('addedBooks', JSON.stringify(storeData));
+  localStorage.setItem("addedBooks", JSON.stringify(storeData));
 }
 
 // Getting values from input fields
 
 // Display books to the UI from local storage
 function displayBooks() {
-  const listOfBooks = document.querySelector('.container');
-  listOfBooks.innerHTML = '';
+  const listOfBooks = document.querySelector(".container");
+  listOfBooks.innerHTML = "";
   storeData.forEach((book, i) => {
-    const bookItem = document.createElement('div');
-    bookItem.className = 'book-item';
-    bookItem.style.listStyleType = 'none'; // add this line to remove the bullet points
+    const bookItem = document.createElement("div");
+    bookItem.className = "book-item";
+    bookItem.style.listStyleType = "none"; // add this line to remove the bullet points
 
-    const bookinfo = document.createElement('div');
-    bookinfo.className = 'book-info';
+    const bookinfo = document.createElement("div");
+    bookinfo.className = "book-info";
     bookinfo.innerHTML = `
       <span>${book.title}</span>
       <span> by ${book.author}</span>
     `;
     bookItem.appendChild(bookinfo);
 
-    const rmvbtn = document.createElement('div');
-    rmvbtn.className = 'remov-button';
+    const rmvbtn = document.createElement("div");
+    rmvbtn.className = "remov-button";
     rmvbtn.innerHTML = `
     <button onclick="removeBook(${i})">Remove</button>
     `;
@@ -40,10 +40,11 @@ function displayBooks() {
   });
 }
 
+const form = document.querySelector("form");
 // Add new data to local storage
-const error = document.querySelector('.error');
+const error = document.querySelector(".error");
 function addNewData(bookTitle, bookAuthor) {
-  if (bookTitle.trim() !== '' && bookAuthor.trim() !== '') {
+  if (bookTitle.trim() !== "" && bookAuthor.trim() !== "") {
     const book = {
       title: bookTitle,
       author: bookAuthor,
@@ -51,16 +52,14 @@ function addNewData(bookTitle, bookAuthor) {
     storeData.push(book);
     updateData();
     displayBooks();
-  }
-  else {
+  } else {
     error.innerHTML = 'Auther or Book Title can"t be empty';
   }
 }
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const title = document.querySelector('.title').value;
-  const author = document.querySelector('.author').value;
+  const title = document.querySelector(".title").value;
+  const author = document.querySelector(".author").value;
   addNewData(title, author);
 });
 
