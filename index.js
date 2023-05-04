b61bdea28d6e5aa1b8499a0f03c50906c985c0a7
Yonas Lemma <lemmayonas@gmail.com>
2023-05-04 10:15:52 +0300

    fix linter issue

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 // Check if local storage is empty, then add an empty array
-if (localStorage.getItem("addedBooks") === null) {
-  localStorage.setItem("addedBooks", JSON.stringify([]));
+if (localStorage.getItem('addedBooks') === null) {
+  localStorage.setItem('addedBooks', JSON.stringify([]));
 }
 
 // Store data into local storage
-const storeData = JSON.parse(localStorage.getItem("addedBooks"));
+const storeData = JSON.parse(localStorage.getItem('addedBooks'));
 
 function updateData() {
-  localStorage.setItem("addedBooks", JSON.stringify(storeData));
+  localStorage.setItem('addedBooks', JSON.stringify(storeData));
 }
 
 // Getting values from input fields
 
 // Display books to the UI from local storage
 function displayBooks() {
-  const listOfBooks = document.querySelector(".container");
-  listOfBooks.innerHTML = "";
+  const listOfBooks = document.querySelector('.container');
+  listOfBooks.innerHTML = '';
   storeData.forEach((book, i) => {
-    const bookItem = document.createElement("div");
-    bookItem.className = "book-item"; // add this line to remove the bullet points
+    const bookItem = document.createElement('div');
+    bookItem.className = 'book-item'; // add this line to remove the bullet points
 
-    const bookinfo = document.createElement("div");
-    bookinfo.className = "book-info";
+    const bookinfo = document.createElement('div');
+    bookinfo.className = 'book-info';
     bookinfo.innerHTML = `
       <span>${book.title}</span>
       <span> by ${book.author}</span>
     `;
     bookItem.appendChild(bookinfo);
 
-    const rmvbtn = document.createElement("div");
-    rmvbtn.className = "remov-button";
+    const rmvbtn = document.createElement('div');
+    rmvbtn.className = 'remov-button';
     rmvbtn.innerHTML = `
     <button onclick="removeBook(${i})">Remove</button>
     `;
@@ -39,11 +39,11 @@ function displayBooks() {
 }
 
 // Add new data to local storage
-const error = document.querySelector(".error");
-const t = document.querySelector(".title");
-const a = document.querySelector(".author");
+const error = document.querySelector('.error');
+const t = document.querySelector('.title');
+const a = document.querySelector('.author');
 function addNewData(bookTitle, bookAuthor) {
-  if (bookTitle.trim() !== "" && bookAuthor.trim() !== "") {
+  if (bookTitle.trim() !== '' && bookAuthor.trim() !== '') {
     const book = {
       title: bookTitle,
       author: bookAuthor,
@@ -51,21 +51,22 @@ function addNewData(bookTitle, bookAuthor) {
     storeData.push(book);
     updateData();
     displayBooks();
-    error.innerHTML = "";
-    t.value = "";
-    a.value = "";
+    error.innerHTML = '';
+    t.value = '';
+    a.value = '';
   } else {
     error.innerHTML = 'Auther or Book Title can"t be empty';
   }
 }
+// eslint-disable-next-line no-unused-vars
 function SubmitBook() {
-  const title = document.querySelector(".title").value;
-  const author = document.querySelector(".author").value;
+  const title = document.querySelector('.title').value;
+  const author = document.querySelector('.author').value;
   addNewData(title, author);
 }
 
-const addbtndiv = document.querySelector(".add-btn-div");
-const addbtn = document.createElement("span");
+const addbtndiv = document.querySelector('.add-btn-div');
+const addbtn = document.createElement('span');
 addbtn.innerHTML = `
 <button onclick={SubmitBook()}>Add</button>`;
 addbtndiv.appendChild(addbtn);
